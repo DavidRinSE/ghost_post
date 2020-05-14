1ff8b13ea3f7c50208272d331b4f4247ac95a55c
DavidRinSE <davidrichardsonin@gmail.com>
2020-05-14 11:32:46 -0400

    Made post model for boast and roast

--- ghost_post/models.py
+++ ghost_post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    isBoast = models.BooleanField()
+    content = models.CharField(max_length=280)
+    upVotes = models.IntegerField()
+    downVotes = models.IntegerField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
+
+
+
+# One model to represent both boasts and roasts
+#     Boolean to tell whether it's a boast or a roast
+#     CharField to put the content of the post in
+#     IntegerField for up votes
+#     IntegerField for down votes
+#     DateTimeField for submission time
