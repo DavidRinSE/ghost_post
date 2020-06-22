d880048eca02cb8ea4f9fbd7eb0b616bfe0a3e8d
DavidRinSE <davidrichardsonin@gmail.com>
2020-06-22 11:53:33 -0400

    Working REST API

--- ghost_post/urls.py
+++ ghost_post/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from ghost_post import views
 
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
